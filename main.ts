9871e4d791d33fb359e44afc909730824760f725
s20221060-sketch <s20221060@kauyan.edu.hk>
2025-11-19 17:46:53 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("Ella")
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Heart)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("Jasmine")
+})
+basic.forever(function () {
+	
+})
